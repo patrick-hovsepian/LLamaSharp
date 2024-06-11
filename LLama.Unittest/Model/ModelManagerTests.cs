1cf6221d250cc5c9322780e54189a745e4590f79
pat_hov <hov@hovbook>
2024-06-10 18:33:29 -0700

    interface

--- LLama.Unittest/Model/ModelManagerTests.cs
+++ LLama.Unittest/Model/ModelManagerTests.cs
@@ -17,23 +17,38 @@ public class ModelManagerTests
         var dirs = TestableModelManager.ModelDirectories;
         Assert.Single(dirs);
 
-        Assert.Equal(Constants.ModelDirectory, dirs.First());
+        var expected = dirs.First()!.Contains(Constants.ModelDirectory);
+        Assert.True(expected);
+    }
+
+    [Fact]
+    public void ModelDirectories_AddDirectory_DoesntDuplicate()
+    {
+        for (var i = 0; i < 10; i++)
+        {
+            TestableModelManager.AddDirectory(Constants.ModelDirectory);
+
+            var dirs = TestableModelManager.ModelDirectories;
+            Assert.Single(dirs);
+            var expected = dirs.First()!.Contains(Constants.ModelDirectory);
+            Assert.True(expected);
+        }
     }
 
     [Fact]
     public void ModelFiles_IsCorrect()
     {
-        var files = TestableModelManager.ModelFiles;
+        var files = TestableModelManager.ModelFileList;
         Assert.Equal(4, files.Count());
     }
 
     [Fact]
     public async Task LoadModel_LoadsAndCaches()
     {
-        var modelToLoad = TestableModelManager.ModelFiles
-            .First(f => f.Contains("llama-2-7b"));
+        var modelToLoad = TestableModelManager.ModelFileList
+            .First(f => f.FileName.Contains("llama-2-7b"));
 
-        var model = await TestableModelManager.Load(modelToLoad, (m) =>
+        var model = await TestableModelManager.LoadModel(modelToLoad.FilePath, (m) =>
         {
             m.GpuLayerCount = 1;
             m.Seed = 12;
@@ -45,6 +60,6 @@ public class ModelManagerTests
         Assert.True(isLoaded);
 
         // unload
-        Assert.True(TestableModelManager.Unload(model.ModelName));
+        Assert.True(TestableModelManager.UnloadModel(model.ModelName));
     }
 }
--- LLama/Model/ModelManager.cs
+++ LLama/Model/ModelManager.cs
@@ -3,28 +3,46 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using LLama.Common;
 
 namespace LLama.Model;
 
-internal class ModelManagerConfiguration
+public enum ModelFileType
 {
-    public IEnumerable<string> ModelDirectories { get; init; } = [];
+    GGUF
+}
+
+public class ModelFileMetadata
+{
+    public string FileName { get; init; } = string.Empty;
+    public string FilePath { get; init; } = string.Empty;
+    public ModelFileType ModelType { get; init; }
+    public long SizeInBytes { get; init; } = 0;
 }
 
 internal interface IModelManager
 {
-    public IEnumerable<string> ModelDirectories { get; set; }
-    public IEnumerable<string> ModelFiles { get; }
+    // Model Directories
+    public IEnumerable<string> ModelDirectories { get; }
+    public void AddDirectory(string directory);
+    public bool RemoveDirectory(string directory);
+    public void RemoveAllDirectories();
 
-    public Task<LLamaWeights> Load(string modelPath,
+    // Model Files
+    public IEnumerable<ModelFileMetadata> ModelFileList { get; }
+    public IEnumerable<ModelFileMetadata> GetAvailableModelsFromDirectory(string directory);
+    public bool TryGetModelFileMetadata(string fileName, out ModelFileMetadata modelMeta);
+
+    // Model Load and Unload
+    public Task<LLamaWeights> LoadModel(string modelPath,
         Action<ModelParams>? modelConfigurator = null!,
         string modelId = "",
         CancellationToken cancellationToken = default);
-    public bool Unload(string modelId);
-    public void UnloadAll();
+    public bool UnloadModel(string modelId);
+    public void UnloadAllModels();
 
     public bool TryGetLoadedModel(string modeId, out LLamaWeights model);
     public IEnumerable<LLamaWeights> GetLoadedModels();
@@ -36,94 +54,156 @@ internal class ModelManager : IModelManager
         ".gguf"
     ];
 
-    private readonly HashSet<string> _modelDirectories = [];
-    private readonly List<string> _modelFiles = [];
+    // keys are directories, values are applicable models
+    private readonly Dictionary<string, IEnumerable<ModelFileMetadata>> _availableModels = [];
 
-    private readonly Dictionary<string, LLamaWeights> _modelCache = [];
+    // model id/alias, to loaded model
+    private readonly Dictionary<string, LLamaWeights> _loadedModelCache = [];
 
     public ModelManager(string[] directories)
     {
         FindAndAddModels(directories);
     }
 
-    private void FindAndAddModels(IEnumerable<string> dirs)
+    private void FindAndAddModels(params string[] dirs)
     {
         foreach (var dir in dirs)
         {
-            if (!Directory.Exists(dir))
+            var fullDirectoryPath = Path.GetFullPath(dir);
+
+            if (!Directory.Exists(fullDirectoryPath))
             {
-                Trace.TraceError($"Model directory {dir} does not exist");
+                Trace.TraceError($"Model directory '{fullDirectoryPath}' does not exist");
+                continue;
+            }
+
+            if (_availableModels.ContainsKey(fullDirectoryPath))
+            {
+                Trace.TraceWarning($"Model directory '{fullDirectoryPath}' already probed");
                 continue;
             }
 
             // find models in current dir that are of expected type
-            _modelDirectories.Add(dir);
-            var modelFiles = Directory.EnumerateFiles(dir)
-                .Where(f =>
+            List<ModelFileMetadata> directoryModelFiles = [];
+            foreach (var file in Directory.EnumerateFiles(fullDirectoryPath))
+            {
+                if (!ExpectedModelFileTypes.Contains(Path.GetExtension(file)))
                 {
-                    var fi = new FileInfo(f);
-                    return ExpectedModelFileTypes.Contains(fi.Extension);
-                });
+                    continue;
+                }
 
-            _modelFiles.AddRange(modelFiles);
+                // expected model file
+                var fi = new FileInfo(file);
+                directoryModelFiles.Add(new ModelFileMetadata
+                {
+                    FileName = fi.Name,
+                    FilePath = fi.FullName,
+                    ModelType = ModelFileType.GGUF,
+                    SizeInBytes = fi.Length,
+                });
+            }
+
+            _availableModels.Add(fullDirectoryPath, directoryModelFiles);
         }
     }
 
-    public IEnumerable<string> ModelFiles => _modelFiles;
+    public IEnumerable<ModelFileMetadata> ModelFileList
+        => _availableModels.SelectMany(x => x.Value);
     public IEnumerable<string> ModelDirectories
+        => _availableModels.Keys;
+
+    public void AddDirectory(string directory)
     {
-        get => _modelDirectories;
-        set
-        {
-            _modelFiles.Clear();
-            FindAndAddModels(value);
-        }
+        FindAndAddModels(directory);
+    }
+
+    public bool RemoveDirectory(string directory)
+    {
+        return _availableModels.Remove(directory);
+    }
+
+    public void RemoveAllDirectories()
+    {
+        _availableModels.Clear();
+    }
+
+    public IEnumerable<ModelFileMetadata> GetAvailableModelsFromDirectory(string directory)
+    {
+        var dirPath = Path.GetFullPath(directory);
+        return _availableModels.TryGetValue(dirPath, out var dirModels)
+            ? dirModels
+            : [];
+    }
+
+    public bool TryGetModelFileMetadata(string fileName, out ModelFileMetadata modelMeta)
+    {
+        var filePath = Path.GetFullPath(fileName);
+        modelMeta = ModelFileList.FirstOrDefault(f => f.FilePath == filePath)!;
+        return modelMeta != null;
     }
 
     public IEnumerable<LLamaWeights> GetLoadedModels()
     {
-        return _modelCache.Values;
+        return _loadedModelCache.Values;
     }
 
     public bool TryGetLoadedModel(string modelId, out LLamaWeights model)
     {
-        return _modelCache.TryGetValue(modelId, out model!);
+        return _loadedModelCache.TryGetValue(modelId, out model!);
     }
 
-    public async Task<LLamaWeights> Load(string modelPath,
+    public async Task<LLamaWeights> LoadModel(string modelPath,
         Action<ModelParams>? modelConfigurator = null!,
         string modelId = "",
         CancellationToken cancellationToken = default)
     {
+        // Configure model params
         var modelParams = new ModelParams(modelPath);
-        modelConfigurator?.Invoke(modelParams);
+        modelConfigurator ??= DefaultModelConfigurator;
+        modelConfigurator.Invoke(modelParams);
 
+        // load and cache
         var model = await LLamaWeights.LoadFromFileAsync(modelParams, cancellationToken);
         if (string.IsNullOrWhiteSpace(modelId))
         {
             modelId = model.ModelName;
         }
-        _modelCache.Add(modelId, model);
+        _loadedModelCache.Add(modelId, model);
         return model;
     }
 
-    public bool Unload(string modelId)
+    public static void DefaultModelConfigurator(ModelParams modelParams)
+    {
+        // Reasonable defaults
+        modelParams.GpuLayerCount = 12;
+        modelParams.Seed = 1337u;
+        modelParams.ContextSize = 2048;
+        modelParams.Encoding = Encoding.UTF8;
+
+        modelParams.UseMemoryLock = true;
+        modelParams.UseMemorymap = true;
+
+        // auto detect
+        modelParams.Threads = null!;
+        modelParams.BatchThreads = null!;
+    }
+
+    public bool UnloadModel(string modelId)
     {
         if (TryGetLoadedModel(modelId, out var model))
         {
             model.Dispose();
-            return true;
+            return _loadedModelCache.Remove(modelId);
         }
-
         return false;
     }
 
-    public void UnloadAll()
+    public void UnloadAllModels()
     {
-        foreach (var model in _modelCache.Values)
+        foreach (var model in _loadedModelCache.Values)
         {
             model.Dispose();
         }
-        _modelCache.Clear();
+        _loadedModelCache.Clear();
     }
 }
