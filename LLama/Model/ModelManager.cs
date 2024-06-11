b9269920bbd60f0eab8bf255a694d9b13d91dea7
Pat Hov <hov@hov.com>
2024-06-10 21:23:30 -0700

    tests

--- LLama/Model/ModelManager.cs
+++ LLama/Model/ModelManager.cs
@@ -10,46 +10,126 @@ using LLama.Common;
 
 namespace LLama.Model;
 
+/// <summary>
+/// Types of supported model files
+/// </summary>
 public enum ModelFileType
 {
     GGUF
 }
 
+/// <summary>
+/// Metadata about available models
+/// </summary>
 public class ModelFileMetadata
 {
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
     public string FileName { get; init; } = string.Empty;
     public string FilePath { get; init; } = string.Empty;
     public ModelFileType ModelType { get; init; }
     public long SizeInBytes { get; init; } = 0;
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
 }
 
-internal interface IModelManager
+/// <summary>
+/// A class that helps organize and load local models
+/// </summary>
+public interface IModelManager
 {
     // Model Directories
+    /// <summary>
+    /// Configured set of directories that are scanned to find local models
+    /// </summary>
+    /// <value></value>
     public IEnumerable<string> ModelDirectories { get; }
+
+    /// <summary>
+    /// Add a directory containing model files
+    /// </summary>
+    /// <param name="directory"></param>
     public void AddDirectory(string directory);
+
+    /// <summary>
+    /// Remove a directory from being scanned and having model files made available
+    /// </summary>
+    /// <param name="directory"></param>
+    /// <returns></returns>
     public bool RemoveDirectory(string directory);
+
+    /// <summary>
+    /// Remove all model directories
+    /// </summary>
     public void RemoveAllDirectories();
 
     // Model Files
+    /// <summary>
+    /// Get all of the model files that are available to be loaded
+    /// </summary>
+    /// <value></value>
     public IEnumerable<ModelFileMetadata> ModelFileList { get; }
+
+    /// <summary>
+    /// Only get the models associated with a specific directory
+    /// </summary>
+    /// <param name="directory"></param>
+    /// <returns>The files, if any associated with a given directory</returns>
     public IEnumerable<ModelFileMetadata> GetAvailableModelsFromDirectory(string directory);
+
+    /// <summary>
+    /// Get the file data for given model
+    /// </summary>
+    /// <param name="fileName"></param>
+    /// <param name="modelMeta"></param>
+    /// <returns>If a model with the given file name is present</returns>
     public bool TryGetModelFileMetadata(string fileName, out ModelFileMetadata modelMeta);
 
     // Model Load and Unload
+    /// <summary>
+    /// Load a model file to be used for infernce
+    /// </summary>
+    /// <param name="modelPath"></param>
+    /// <param name="modelConfigurator"></param>
+    /// <param name="modelId"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns>The loaded model on success</returns>
     public Task<LLamaWeights> LoadModel(string modelPath,
         Action<ModelParams>? modelConfigurator = null!,
         string modelId = "",
         CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Unload and dispose of a model with the given id
+    /// </summary>
+    /// <param name="modelId"></param>
+    /// <returns></returns>
     public bool UnloadModel(string modelId);
+
+    /// <summary>
+    /// Unload all currently loaded models
+    /// </summary>
     public void UnloadAllModels();
 
+    /// <summary>
+    /// Attempt to get a model that's expected to be loaded
+    /// </summary>
+    /// <param name="modeId"></param>
+    /// <param name="model"></param>
+    /// <returns></returns>
     public bool TryGetLoadedModel(string modeId, out LLamaWeights model);
+
+    /// <summary>
+    /// Currently loaded models
+    /// </summary>
+    /// <returns></returns>
     public IEnumerable<LLamaWeights> GetLoadedModels();
 }
 
-internal class ModelManager : IModelManager
+/// <inheritdoc />
+public class ModelManager : IModelManager
 {
+    /// <summary>
+    /// Support model type files
+    /// </summary>
     public static string[] ExpectedModelFileTypes = [
         ".gguf"
     ];
@@ -60,12 +140,16 @@ internal class ModelManager : IModelManager
     // model id/alias, to loaded model
     private readonly Dictionary<string, LLamaWeights> _loadedModelCache = [];
 
+    /// <summary>
+    /// Create a new model manager that seeds available models from the given directory list
+    /// </summary>
+    /// <param name="directories"></param>
     public ModelManager(string[] directories)
     {
-        FindAndAddModels(directories);
+        GetModelsFromDirectories(directories);
     }
 
-    private void FindAndAddModels(params string[] dirs)
+    private void GetModelsFromDirectories(params string[] dirs)
     {
         foreach (var dir in dirs)
         {
@@ -107,26 +191,32 @@ internal class ModelManager : IModelManager
         }
     }
 
+    /// <inheritdoc />
     public IEnumerable<ModelFileMetadata> ModelFileList
         => _availableModels.SelectMany(x => x.Value);
+    /// <inheritdoc />
     public IEnumerable<string> ModelDirectories
         => _availableModels.Keys;
 
+    /// <inheritdoc />
     public void AddDirectory(string directory)
     {
-        FindAndAddModels(directory);
+        GetModelsFromDirectories(directory);
     }
 
+    /// <inheritdoc />
     public bool RemoveDirectory(string directory)
     {
-        return _availableModels.Remove(directory);
+        return _availableModels.Remove(Path.GetFullPath(directory));
     }
 
+    /// <inheritdoc />
     public void RemoveAllDirectories()
     {
         _availableModels.Clear();
     }
 
+    /// <inheritdoc />
     public IEnumerable<ModelFileMetadata> GetAvailableModelsFromDirectory(string directory)
     {
         var dirPath = Path.GetFullPath(directory);
@@ -135,6 +225,7 @@ internal class ModelManager : IModelManager
             : [];
     }
 
+    /// <inheritdoc />
     public bool TryGetModelFileMetadata(string fileName, out ModelFileMetadata modelMeta)
     {
         var filePath = Path.GetFullPath(fileName);
@@ -142,16 +233,19 @@ internal class ModelManager : IModelManager
         return modelMeta != null;
     }
 
+    /// <inheritdoc />
     public IEnumerable<LLamaWeights> GetLoadedModels()
     {
         return _loadedModelCache.Values;
     }
 
+    /// <inheritdoc />
     public bool TryGetLoadedModel(string modelId, out LLamaWeights model)
     {
         return _loadedModelCache.TryGetValue(modelId, out model!);
     }
 
+    /// <inheritdoc />
     public async Task<LLamaWeights> LoadModel(string modelPath,
         Action<ModelParams>? modelConfigurator = null!,
         string modelId = "",
@@ -172,6 +266,10 @@ internal class ModelManager : IModelManager
         return model;
     }
 
+    /// <summary>
+    /// Updates the passed in model params with default values
+    /// </summary>
+    /// <param name="modelParams"></param>
     public static void DefaultModelConfigurator(ModelParams modelParams)
     {
         // Reasonable defaults
@@ -188,6 +286,7 @@ internal class ModelManager : IModelManager
         modelParams.BatchThreads = null!;
     }
 
+    /// <inheritdoc />
     public bool UnloadModel(string modelId)
     {
         if (TryGetLoadedModel(modelId, out var model))
@@ -198,6 +297,7 @@ internal class ModelManager : IModelManager
         return false;
     }
 
+    /// <inheritdoc />
     public void UnloadAllModels()
     {
         foreach (var model in _loadedModelCache.Values)
